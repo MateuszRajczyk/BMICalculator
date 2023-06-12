cacd96ec21c8c1a7cc952629e2f8532ce3618459
Mateusz Rajczyk <mateuszrajczyk96@gmail.com>
2023-06-12 13:58:02 +0200

    refactor code, build new widget ReusableCard with Container

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -20,54 +20,34 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                    child: ReuseableCard(
+                      color: const Color(0xFF1D1E33),
                     ),
                   ),
                   Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                    child: ReuseableCard(
+                      color: const Color(0xFF1D1E33),
                     ),
                   ),
                 ],
               ),
             ),
             Expanded(
-              child: Container(
-                margin: const EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: const Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
+              child: ReuseableCard(
+                color: const Color(0xFF1D1E33),
               ),
             ),
             Expanded(
               child: Row(
                 children: [
                   Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                    child: ReuseableCard(
+                      color: const Color(0xFF1D1E33),
                     ),
                   ),
                   Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                    child: ReuseableCard(
+                      color: const Color(0xFF1D1E33),
                     ),
                   ),
                 ],
@@ -75,5 +55,22 @@ class _InputPageState extends State<InputPage> {
             ),
           ],
         ));
+  }
+}
+
+class ReuseableCard extends StatelessWidget {
+  ReuseableCard({required this.color, super.key});
+
+  Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
   }
 }
