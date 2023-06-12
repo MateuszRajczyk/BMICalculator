a99bbb8bdcaaf25b737b2bde0a27638ad9ca5746
Mateusz Rajczyk <mateuszrajczyk96@gmail.com>
2023-06-12 13:33:39 +0200

    rename app name , add app theme in main.dart, refactor code, separate classes in separate files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'input_page.dart';
+
 void main() => runApp(const BMICalculator());
 
 class BMICalculator extends StatelessWidget {
@@ -7,33 +9,14 @@ class BMICalculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
-
-  @override
-  State<InputPage> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI CALCULATOR'),
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.dark().copyWith(
+          primary: const Color(0xFF0A0E21),
+        ),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
       ),
-      body: const Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
+      home: const InputPage(),
     );
   }
 }
