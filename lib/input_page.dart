50ea300a1d64a2321394ddf3ca6404e863712c71
Mateusz Rajczyk <mateuszrajczyk96@gmail.com>
2023-06-15 11:26:11 +0200

    add theme to the slider and move styling to theme slider widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -94,17 +94,27 @@ class _InputPageState extends State<InputPage> {
                         ),
                       ],
                     ),
-                    Slider(
-                      value: height.toDouble(),
-                      min: 120.0,
-                      max: 220.0,
-                      activeColor: const Color(0xFFEB1555),
-                      inactiveColor: const Color(0xFF8D8E98),
-                      onChanged: (double newValue) {
-                        setState(() {
-                          height = newValue.round();
-                        });
-                      },
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        inactiveTrackColor: const Color(0xFF8D8E98),
+                        activeTrackColor: Colors.white,
+                        overlayColor: const Color(0x29EB1555),
+                        thumbColor: const Color(0xFFEB1555),
+                        thumbShape: const RoundSliderThumbShape(
+                            enabledThumbRadius: 15.0),
+                        overlayShape:
+                            const RoundSliderOverlayShape(overlayRadius: 30.0),
+                      ),
+                      child: Slider(
+                        value: height.toDouble(),
+                        min: 120.0,
+                        max: 220.0,
+                        onChanged: (double newValue) {
+                          setState(() {
+                            height = newValue.round();
+                          });
+                        },
+                      ),
                     ),
                   ],
                 ),
